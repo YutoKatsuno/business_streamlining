db5c70934c07f250d4ac43b6e38d354b5cd1ecb3
Yuto Katsuno <theninja0106@gmail.com>
2023-05-27 13:53:31 +0900

    簡単な自作rpa

--- original/rpa/create_file.py
+++ original/rpa/create_file.py
@@ -1,18 +1,16 @@
 import pyautogui
 import time
 
-pyautogui.hotkey("ctrl", "command", "t")
-time.sleep(3)
-# # ファイルを作成する
-# pyautogui.moveTo(x=658, y=869)
-# pyautogui.click()
+# ファイルを作成する
+pyautogui.moveTo(x=658, y=869)
+pyautogui.click()
 pyautogui.write("touch new.py")
 pyautogui.press("enter")
-#
-# # 3秒待機する
-# time.sleep(3)
-#
-# # ファイルを削除する
-# pyautogui.write("rm new.py")
-# pyautogui.press("enter")
-# print(pyautogui.position())
+
+# 3秒待機する
+time.sleep(3)
+
+# ファイルを削除する
+pyautogui.write("rm new.py")
+pyautogui.press("enter")
+print(pyautogui.position())
